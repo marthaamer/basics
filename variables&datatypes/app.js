a4445ff6a2ee7413a27023b2c445ba4ad8eab118
Mart Haamer <mart.haamer@voco.ee>
2022-09-21 10:34:31 +0300

    Esimene

---
+++ variables&datatypes/app.js
@@ -0,0 +1,43 @@
+/**
+ * documentation
+ * comment
+ * style
+ *
+ *
+ * variables
+ *
+ * let variableName = value; // mutable
+ * const variableName = value; // immutable
+ *
+ */
+
+let numberOne = 5;
+let numberTwo = 2.5;
+const season = "Autumn"
+
+// simple comment
+
+/*
+console.log("first row")
+console.log("second row")
+console.log("third row")
+
+ */
+
+console.log(numberOne)
+console.log(numberTwo)
+console.log(season)
+
+let aasta = 2016;
+let president = "Kersti Kaljulaid"
+let lauseKeskosa = ". aastal valiti Eesti presidentiks "
+let lause = aasta + lauseKeskosa + president
+
+console.log(lause)
+
+let astmeAlus = 2
+let astendaja = 4
+
+let tulemus = Math
+
+console.log()
