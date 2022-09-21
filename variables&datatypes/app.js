c59d4eed6848628d79eb248d2f5046ae97b05ae8
Mart Haamer <mart.haamer@voco.ee>
2022-09-21 11:13:09 +0300

    ÜL 1.4 Trahv

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,47 +1,11 @@
-/**
- * documentation
- * comment
- * style
- *
- *
- * variables
- *
- * let variableName = value; // mutable
- * const variableName = value; // immutable
- *
- */
+// Andmete sisestamine
+let userName = String(prompt("Sisestage nimi: "));
+let lubatudKiirus = Number(prompt("Sisetage lubatud kiirus: "));
+let tegelikKiirus = Number(prompt("Sisetage tegelik kiirus: "));
 
-let numberOne = 5;
-let numberTwo = 2.5;
-const season = "Autumn"
+// Arvutamine
+let trahviSumma = (tegelikKiirus - lubatudKiirus) * 3;
+trahviSumma = Math.min(trahviSumma, 190);
 
-// simple comment
-
-/*
-console.log("first row")
-console.log("second row")
-console.log("third row")
-
- */
-
-console.log(numberOne)
-console.log(numberTwo)
-console.log(season)
-
- // Tund 9/21/2022
-
-let aasta = 2016;
-let president = "Kersti Kaljulaid"
-let lauseKeskosa = ". aastal valiti Eesti presidentiks "
-let lause = aasta + lauseKeskosa + president
-
-console.log(lause)
-
-// kasutaja sisend
-let astmeAlus = Number(prompt("Sisestage astme alus: "));
-let astendaja = Number(prompt("Sisestage astendaja: "));
-
-// arvutame tulemuse
-let tulemus = Math.pow(astmeAlus, astendaja)
-
-console.log(tulemus)
+// Tulemuse välastamine
+console.log(`${userName}, kiiruse ületamise eest on teie trahv ${trahviSumma} eurot.`)
