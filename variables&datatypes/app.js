88fbbb7c1ee29a4f52747ee2a51f0db2dd7c186c
Mart Haamer <marthaamer123@gmail.com>
2022-10-05 21:18:40 +0300

    3.4 Õunte jagamine

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,6 +1,10 @@
-let numDice = Number(prompt('Sisestage täringute arv.'))
+let numAskers = Number(prompt("Mitu pöialpoissi tahab õunu? Kuni 7"))
+const maxApples = 14;
 
-console.log(`Täringute arv: ${numDice}`)
-for (let i = 0; i < numDice; i++) {
-    console.log(Math.floor((Math.random() * 6) + 1))
+let totalApples = 0;
+for (let i = 0; i < numAskers; i++) {
+    let apples = Math.floor(Math.random() * 2) + 1; totalApples += apples;
+    console.log(apples)
 }
+
+console.log("Lumivalgeksele jäi " + (maxApples - totalApples))
