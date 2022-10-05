23f1f9978f04ce12c7fcb0521f360759a1134112
Mart Haamer <marthaamer123@gmail.com>
2022-10-05 21:09:07 +0300

    3.3 Täringumäng

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,15 +1,6 @@
-let inputCycleCount = Number(prompt('Sisestage ringide arv.'))
+let numDice = Number(prompt('Sisestage täringute arv.'))
 
-if ((inputCycleCount % 2) == 1) {
-    inputCycleCount -= 1;
+console.log(`Täringute arv: ${numDice}`)
+for (let i = 0; i < numDice; i++) {
+    console.log(Math.floor((Math.random() * 6) + 1))
 }
-
-let totalCycles = inputCycleCount / 2;
-let cycle = 0;
-
-let totalCarrots = 0; let carrots = 2
-while (cycle < totalCycles) {
-    totalCarrots += carrots; carrots += 2; cycle +=1;
-}
-
-console.log(`Progandite koguarv on: ${totalCarrots}`)
