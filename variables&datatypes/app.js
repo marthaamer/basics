b91b2f37a6c06a05488e882cd08bd352f8849fde
Mart Haamer <mart.haamer@voco.ee>
2022-10-05 09:56:56 +0300

    ÜL 3.2 Jänesevanemate mure

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,5 +1,15 @@
-let mituKordaAratada = Number(prompt('Mitu korda äratada?'))
+let inputCycleCount = Number(prompt('Sisestage ringide arv.'))
 
-for (let i = 0; i < mituKordaAratada; i++) {
-    console.log('Tõuse ja sära!')
+if ((inputCycleCount % 2) == 1) {
+    inputCycleCount -= 1;
 }
+
+let totalCycles = inputCycleCount / 2;
+let cycle = 0;
+
+let totalCarrots = 0; let carrots = 2
+while (cycle < totalCycles) {
+    totalCarrots += carrots; carrots += 2; cycle +=1;
+}
+
+console.log(`Progandite koguarv on: ${totalCarrots}`)
